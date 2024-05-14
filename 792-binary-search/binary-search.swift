ac67ba50ed65a92362ce7673cbfd4291d9369697
Georgii Davydenko <109753005+Gadave@users.noreply.github.com>
2024-05-14 21:55:23 +0300

    Time: 105 ms (33.86%) | Memory: 15.6 MB (32.81%) - LeetSync

--- 792-binary-search/binary-search.swift
+++ 792-binary-search/binary-search.swift
@@ -1,15 +1,19 @@
 class Solution {
     func search(_ nums: [Int], _ target: Int) -> Int {
-        var cur = -1
-        for num in nums {
-            cur += 1
-            if num == target {
-                return cur
-            } else if num > target {
-                return -1
+        var slow = 0
+        var fast = nums.count - 1
+
+        while slow <= fast {
+            let mid = (slow + fast) / 2
+            if nums[mid] == target {
+                return mid
+            }
+            if nums[mid] < target {
+                slow = mid + 1
+            } else {
+                fast = mid - 1
             }
         }
-
         return -1
     }
 }
